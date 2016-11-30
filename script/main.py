1aaf2ed18d99ecb246e13a825f18ae7bcb2e6386
Sergent Julien <sergent.julien.1@gmail.com>
2016-11-30 14:31:46 +0100

    Update the copy of file : each file of conponent are copy in one directory

--- script/main.py
+++ script/main.py
@@ -14,11 +14,14 @@ def copy_script():
         file = open('./'+conponent+'/hostfile.txt')
         for host in file:
             print('[Info] Transfer on marchine : ' + host.replace('\n', ''))
+            os.system('ssh -i ~/.ssh/xnet xnet@' + host.replace('\n', '') + ' \'rm -rf ' + conponent + ' \'')
+            os.system('ssh -i ~/.ssh/xnet xnet@' + host.replace('\n', '') + ' \'mkdir ' + conponent + ' \'')
+            os.system('scp -p -i ~/.ssh/xnet ./install_config_machine.py xnet@' + host.replace('\n','') + ':~')
             for fichier in os.listdir('./'+conponent):
-                os.system('scp -p -i ~/.ssh/xnet ./'+conponent+'/'+fichier+' xnet@' + host.replace('\n', '') + ':~')
+                os.system('scp -p -i ~/.ssh/xnet ./'+conponent+'/'+fichier+' xnet@' + host.replace('\n', '') + ':~/'+conponent+'/')
             print('[Info] Launch script of install on marchine : ' + host.replace('\n', ''))
             os.system('ssh -i ~/.ssh/xnet xnet@'+host.replace('\n','')+' \'./install_config_machine.py\'')
-            os.system('ssh -i ~/.ssh/xnet xnet@'+host.replace('\n','')+' \'./install_'+conponent+'.py\'')
+            os.system('ssh -i ~/.ssh/xnet xnet@'+host.replace('\n','')+' \'./'+conponent+'/install_'+conponent+'.py\'')
         file.close()
     return
 
