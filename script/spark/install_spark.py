f9c1a0c2235e62b5b5c80875a16143ca70583e41
Sergent Julien <sergent.julien.1@gmail.com>
2016-12-01 00:14:42 +0100

    Update script by using logging and replace os.system by subprocess.run for spark 1 master and two worker launch automaticaly TODO ; specify full conf for spark

--- script/spark/install_spark.py
+++ script/spark/install_spark.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import os
+import logging
+import subprocess
 
 spark_version = 'spark-2.0.2-bin-hadoop2.7'
 
@@ -9,19 +11,29 @@ spark_version = 'spark-2.0.2-bin-hadoop2.7'
 def install_spark():
     SPARK_STATUS = os.popen('stop-master.sh 2>&1 ', "r").read()
     if 'not found' in SPARK_STATUS:
-        os.system(' echo "[Info] Downloading Spark ..."')
-        os.system('wget -q http://d3kbcqa49mib13.cloudfront.net/'+spark_version+'.tgz')
-        os.system(' echo "[Info] Installation of Spark ..."')
-        os.system('sudo rm -rf /usr/lib/spark')
-        os.system('sudo mkdir /usr/lib/spark')
-        os.system('sudo tar xf '+spark_version+'.tgz -C /usr/lib/spark/')
-        os.system('rm '+spark_version+'.tgz')
+        logging.info(" Downloading Spark ...")
+        out = subprocess.run(['wget','-q','http://d3kbcqa49mib13.cloudfront.net/'+spark_version+'.tgz'], check=True)
+        if out.returncode == 0:
+            logging.info(" Downloading Spark with success")
+        logging.info(" Installation of Spark ...")
+        subprocess.run(['sudo','rm','-rf','/usr/lib/spark'])
+        subprocess.run(['sudo','mkdir','/usr/lib/spark'])
+        out = subprocess.run(['sudo','tar','-xf',spark_version+'.tgz','-C','/usr/lib/spark'], check=True)
+        if out.returncode == 0:
+            logging.info(" Uncompressing Spark with success")
+        subprocess.run(['rm',spark_version+'.tgz'])
+        # TODO use subprocess
+        #subprocess.run(['echo','export PATH=$PATH:/usr/lib/spark/'+spark_version+'/sbin/','>>','~/.profile'])
+        #subprocess.run(['echo','export SPARK_HOME=/usr/lib/spark/'+spark_version+'','>>','~/.profile'])
         os.system('echo "export PATH=$PATH:/usr/lib/spark/'+spark_version+'/sbin/" >> ~/.profile')
         os.system('echo "export SPARK_HOME=/usr/lib/spark/'+spark_version+'" >> ~/.profile')
-        os.system('sudo mkdir /usr/lib/spark/spark-2.0.2-bin-hadoop2.7/logs')
-        os.system('sudo chmod 777 -R /usr/lib/spark/spark-2.0.2-bin-hadoop2.7/logs/')
-        os.system('sudo mkdir /usr/lib/spark/spark-2.0.2-bin-hadoop2.7/work')
-        os.system('sudo chmod 777 -R /usr/lib/spark/spark-2.0.2-bin-hadoop2.7/work/')
+        subprocess.run(['sudo','mkdir','/usr/lib/spark/spark-2.0.2-bin-hadoop2.7/logs'])
+        subprocess.run(['sudo','chmod','777','-R','/usr/lib/spark/spark-2.0.2-bin-hadoop2.7/logs'])
+        subprocess.run(['sudo','mkdir','/usr/lib/spark/spark-2.0.2-bin-hadoop2.7/work'])
+        subprocess.run(['sudo','chmod','777','-R','/usr/lib/spark/spark-2.0.2-bin-hadoop2.7/work'])
+        # TODO add log for know if spark is installed
     return
 
-install_spark()
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    install_spark()
--- script/spark/launch_spark.py
+++ script/spark/launch_spark.py
@@ -1,6 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import os
+import logging
+import subprocess
 
 url_master = '127.0.0.1'
 port_master = '7070'
@@ -11,30 +13,39 @@ def launch_spark():
     ip = os.popen('ifconfig ens3 | grep "inet ad" | cut -f2 -d: | awk \'{print $1}\'', "r").read()
     file = open('./spark/master.txt')
     for host in file:
-        if host + '\n' == ip:
-            url_master = host
-            launch_master()
+        if host in ip:
+            url_master = host.split('/n')[0]
+            launch_master(url_master)
     file.close()
     file = open('./spark/slave.txt')
     for host in file:
-        if host + '\n' == ip:
+        if host in ip:
             launch_slave()
     file.close()
     return
 
 # Function for launch master
-def launch_master():
-    os.system(' echo "[Info] Start Spark Master ...."')
-    os.system('stop-master.sh')
-    #os.system('start-master.sh -i '+url_master+' -p '+port_master+' --webui-port '+port_webui)
+def launch_master(url):
+    logging.info(" Start Spark Master ...")
+    subprocess.run(['stop-master.sh'])
+    subprocess.run(['start-master.sh','-i',url,'-p',port_master,'--webui-port',port_webui])
     return
 
 
 # Function for launch slave
 def launch_slave():
-    os.system(' echo "[Info] Start Spark Worker ...."')
-    os.system('stop-slave.sh')
-    # os.system('start-slave.sh spark://'+url_master+':'+port_master)
+    logging.info(" Start Spark Worker ...")
+    subprocess.run(['stop-slave.sh'])
+    subprocess.run(['start-slave.sh','spark://'+get_Ip_Master()+':'+port_master])
     return
 
-launch_spark()
+# Function for recover the address of master
+def get_Ip_Master():
+    file = open('./spark/master.txt')
+    for host in file:
+        ip_master = host.split('/n')[0]
+        return ip_master
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    launch_spark()
