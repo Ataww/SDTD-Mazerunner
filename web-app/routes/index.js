0c7f43a9d9ef9d98a6b08ff5ed96681490396caf
Sergent Julien <sergent.julien.1@gmail.com>
2016-12-02 23:09:27 +0100

    Remove page about spark test

--- web-app/routes/index.js
+++ web-app/routes/index.js
@@ -6,9 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'SDTD-Mazerunner' });
 });
 
-/* GET spark page. */
-router.get('/spark', function(req, res, next) {
-    res.render('spark', { title: 'SDTD-Mazerunner' });
-});
-
 module.exports = router;
