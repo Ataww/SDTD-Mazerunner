c4946612114c8da1ca9456b0dee0cbdbfa528d3b
taboulot <clement-taboulot@hotmail.fr>
2016-12-05 21:37:43 +0100

    RabbitMQ : Add script to test sending and receving

---
+++ script/rabbitmq/test/send.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import argparse, pika
+
+parser = argparse.ArgumentParser()
+parser.add_argument('host', help="RabbitMQ hostname")
+parser.add_argument('user', help="RabbitMQ user")
+parser.add_argument('password', help="RabbitMQ password")
+args = parser.parse_args()
+
+credentials = pika.PlainCredentials(args.user, args.password)
+connection = pika.BlockingConnection(pika.ConnectionParameters(args.host,5672,'/', credentials))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello World!')
+print(" [x] Sent 'Hello World!'")
+connection.close()
