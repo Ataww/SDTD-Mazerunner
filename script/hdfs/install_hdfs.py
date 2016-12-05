c902fd6e4bef996c1754eefbdac9ba346c33872f
Vincent Chenal <vince.chenal@gmail.com>
2016-12-05 21:41:25 +0100

    Update HDFS installation script

--- script/hdfs/install_hdfs.py
+++ script/hdfs/install_hdfs.py
@@ -4,27 +4,30 @@ import subprocess, os, sys, logging
 
 version='hadoop-2.7.3'
 distrib='http://apache.crihan.fr/dist/hadoop/common/'+version+'/'+version+'.tar.gz'
-
+conf_dir = "/opt/"+version+"/etc/hadoop/"
+conf_dir_export = "export HADOOP_CONF_DIR="+conf_dir
 
 def install_hdfs():
 	"""Install hadoop et set it up"""
 	logging.info('Downloading hadoop')
-	subprocess.run(['wget', distrib], check=True)
+	subprocess.run(['wget', "-nc", distrib], check=True)
 	logging.info('Uncompressing to /opt/')
 	subprocess.run(['tar', 'xf', version+'.tar.gz', '-C', '/opt/'], check=True)
 	logging.info('Creating symbolic links')
 	subprocess.run(['ln', '-s', '/opt/'+version+'/bin/hdfs', '/bin/hdfs'])
-	subprocess.run(['ln', '-s', '/opt/'+version+'/sbin/start-dfs.sh', '/sbin/start-dfs.sh'])
-	subprocess.run(['ln', '-s', '/opt/'+version+'/sbin/stop-dfs.sh', '/sbin/stop-dfs.sh'])
+	#subprocess.run(['ln', '-s', '/opt/'+version+'/sbin/start-dfs.sh', '/sbin/start-dfs.sh'])
+	#subprocess.run(['ln', '-s', '/opt/'+version+'/sbin/stop-dfs.sh', '/sbin/stop-dfs.sh'])
 	logging.info('Setting environment variables')
-	with open(os.path.expanduser('~/.profile'), 'a') as proFile:
-		subprocess.run(['echo', 'export HADOOP_CONF_DIR=/opt/etc/hadoop'], stdout=proFile, check=True)
-		subprocess.run(['source', '~/.profile'])
+	with open(os.path.expanduser('~/.profile'), 'r+') as proFile:
+		if conf_dir_export not in proFile.read():
+			subprocess.run(['echo', conf_dir_export], stdout=proFile, check=True)
+			#I got 'source : no such file or directory' and we do not need to source from this script
+			#subprocess.run(['source', '~/.profile'])
 	logging.info('Cleaning up')
-	subprocess.run(['rm', '-rf', version+'.tar.gz'])	
+	#rm is not necessary if you want to re-download just rm the tar.gz and relaunch the script
+    #subprocess.run(['rm', '-rf', version+'.tar.gz'])
 	logging.info('Copying HDFS configuration files')
-	subprocess.run(['cp', '-r', './etc/hadoop/', '/opt/'+version+'/etc/'])
-
+	subprocess.run('cp -r ./etc/hadoop/* /opt/'+version+'/etc/hadoop', shell=True)
 
 def install_zookeeper():
 	pass
