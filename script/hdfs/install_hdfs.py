1ed941f626a17e789ab03951f00c5cbc1f4d76a3
Valentin Matton <taq@protonmail.ch>
2016-12-06 15:41:43 +0100

    hdfs: move install to xnet home

--- script/hdfs/install_hdfs.py
+++ script/hdfs/install_hdfs.py
@@ -4,38 +4,38 @@ import subprocess, os, sys, logging
 
 version='hadoop-2.7.3'
 distrib='http://apache.crihan.fr/dist/hadoop/common/'+version+'/'+version+'.tar.gz'
-conf_dir = "/opt/"+version+"/etc/hadoop/"
+
+hadoop_prefix = '/home/xnet/'+version
+
+conf_dir = hadoop_prefix+"/etc/hadoop"
 conf_dir_export = "export HADOOP_CONF_DIR="+conf_dir
+
+
+zk_version='zookeeper-3.4.9'
+zk_distrib='http://apache.crihan.fr/dist/zookeeper/'+zk_version+'/'+version+'tar.gz'
+
 
 def install_hdfs():
 	"""Install hadoop et set it up"""
 	logging.info('Downloading hadoop')
 	subprocess.run(['wget', "-nc", distrib], check=True)
-	logging.info('Uncompressing to /opt/')
-	subprocess.run(['tar', 'xf', version+'.tar.gz', '-C', '/opt/'], check=True)
-	logging.info('Creating symbolic links')
-	subprocess.run(['ln', '-s', '/opt/'+version+'/bin/hdfs', '/bin/hdfs'])
-	#subprocess.run(['ln', '-s', '/opt/'+version+'/sbin/start-dfs.sh', '/sbin/start-dfs.sh'])
-	#subprocess.run(['ln', '-s', '/opt/'+version+'/sbin/stop-dfs.sh', '/sbin/stop-dfs.sh'])
+	logging.info('Uncompressing to /home/xnet')
+	subprocess.run(['tar', 'xf', version+'.tar.gz', '-C', '/home/xnet'], check=True)
 	logging.info('Setting environment variables')
 	with open(os.path.expanduser('~/.profile'), 'r+') as proFile:
 		if conf_dir_export not in proFile.read():
 			subprocess.run(['echo', conf_dir_export], stdout=proFile, check=True)
-			#I got 'source : no such file or directory' and we do not need to source from this script
-			#subprocess.run(['source', '~/.profile'])
-	logging.info('Cleaning up')
-	#rm is not necessary if you want to re-download just rm the tar.gz and relaunch the script
-    #subprocess.run(['rm', '-rf', version+'.tar.gz'])
+			subprocess.run(['echo', 'export HADOOP_PREFIX='+hadoop_prefix], stdout=proFile, check=True)	
 	logging.info('Copying HDFS configuration files')
-	subprocess.run('cp -r ./etc/hadoop/* /opt/'+version+'/etc/hadoop', shell=True)
+	# files to copy should be somewhere with the installation script
+	# for now it uses a local repo
+	subprocess.run('cp SDTD-Mazerunner/script/hdfs/etc/hadoop/* /home/xnet/'+version+'/etc/hadoop', shell=True)
+
 
 def install_zookeeper():
 	pass
 
 
 if __name__ == '__main__':
-	if os.getuid() != 0:
-		print('Root permissions required', file=sys.stderr)
-		sys.exit(1)
 	logging.basicConfig(level=logging.INFO)
 	install_hdfs()
