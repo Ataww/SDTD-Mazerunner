08b0c63b92edc263521fea3a085eece637d46d81
Sergent Julien <sergent.julien.1@gmail.com>
2016-12-08 00:35:29 +0100

    modify script for stop rabbitmq

--- script/rabbitmq/stop_rabbitmq.py
+++ script/rabbitmq/stop_rabbitmq.py
@@ -2,7 +2,7 @@
 import subprocess, logging
 
 def stop_rabbitmq():
-    subprocess.run(['sudo', 'rabbitmqctl', 'start_app'])
+    subprocess.run(['sudo', 'rabbitmqctl', 'stop_app'])
     return
 
 if __name__ == '__main__':
