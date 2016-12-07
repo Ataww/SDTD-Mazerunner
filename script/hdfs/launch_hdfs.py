df9ae10104725f911e942312415c0713e51a6105
Valentin Matton <taq@protonmail.ch>
2016-12-07 10:36:54 +0100

    hdfs: launch script and env

--- script/hdfs/launch_hdfs.py
+++ script/hdfs/launch_hdfs.py
@@ -1,1 +1,26 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
+
+import logging, sys, subprocess
+
+
+version='hadoop-2.7.3'
+
+
+def format():
+	logging.info('Formatting HDFS namenode')
+	subprocess.run(['/home/xnet/'+version+'/sbin/hdfs', 'namenode', '-format'], check=True)
+
+
+def launch():
+	logging.info('Launching HDFS cluster')
+	subprocess.run(['/home/xnet/'+version+'/sbin/start-dfs.sh'], check=True)
+
+
+if __name__ == '__main__':
+	logging.basicConfig(level=logging.INFO)
+	
+	if len(sys.argv) > 2:
+		if sys.argv[1] == '-f':
+			format()
+
+	launch()
