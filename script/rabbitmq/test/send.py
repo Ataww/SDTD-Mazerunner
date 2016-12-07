e7ea0d85476054b55750d892a1aaaec3a60e4649
Maxime Hagenbourger <maximehg@gmail.com>
2016-12-07 10:50:12 +0100

    modif send.py for it to be durable

--- script/rabbitmq/test/send.py
+++ script/rabbitmq/test/send.py
@@ -16,7 +16,10 @@ channel.queue_declare(queue='hello', durable=True)
 
 channel.basic_publish(exchange='',
                       routing_key='hello',
-                      body='Hello World!')
+                      body='Hello World!',
+                      properties=pika.BasicProperties(
+                        delivery_mode = 2,
+                      ))
 
 print(" [x] Sent 'Hello World!'")
 connection.close()
