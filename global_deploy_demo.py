9fdefc5ebfe76f9d07f192a2e764cbd7e0b2d55b
Sergent Julien <sergent.julien.1@gmail.com>
2016-12-08 23:25:22 +0100

    global script for launch the deployment of all service and deployment of website

---
+++ global_deploy_demo.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import logging
+import subprocess
+
+# Function for launch demo
+def launch_demo():
+    print("#############################################################")
+    print("############### DEMO SDTD-Mazerunner deploiment #############")
+    print("#############################################################")
+    subprocess.run(['./launch_deployment.py'], cwd='./script/')
+    subprocess.run(['./deploy_web_site.py'],cwd='./web-app/script/')
+
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO,format="%(asctime)s :: %(levelname)s :: %(message)s")
+    launch_demo()
