f91480225b45cdb2f1654de4f082e60f0b754209
Nathanael Couret <nathanael.couret@gmail.com>
2017-01-17 12:17:58 +0100

    Added exception handler to orchestrator on rmq channel

--- backend/src/main/scala/JobOrchestration/JobOrchestration.scala
+++ backend/src/main/scala/JobOrchestration/JobOrchestration.scala
@@ -1,12 +1,12 @@
 package JobOrchestration
 
-import java.io._
-
 import com.rabbitmq.client.AMQP.BasicProperties
-import com.rabbitmq.client.{BuiltinExchangeType, ConnectionFactory, DefaultConsumer, Envelope}
+import com.rabbitmq.client._
 import org.apache.spark.launcher.SparkLauncher
 import com.typesafe.config._
 import org.apache.spark.SparkContext
+
+import scala.util.control.Breaks
 
 object JobOrchestration {
 
@@ -27,8 +27,9 @@ object JobOrchestration {
   cf.setPort(LOADBALANCER_PORT)
   cf.setUsername(RMQ_USER)
   cf.setPassword(RMQ_PWD)
+  cf.setExceptionHandler(new CustomExceptionHandler)
 
-  val sparkLauncher = new SparkLauncher()
+  val sparkLauncher : SparkLauncher = new SparkLauncher()
     .setAppResource(LAUNCHER_JAR)
     .setMainClass(MAINCLASS_LAUNCHER)
     .setMaster(LAUNCHER_MASTER)
@@ -36,8 +37,10 @@ object JobOrchestration {
   val context = new SparkContext()
 
   def main(args: Array[String]): Unit = {
+    createChannel()
+  }
 
-
+  def createChannel() : Unit = {
     // Create connection
     val conn = cf.newConnection
 
@@ -45,10 +48,10 @@ object JobOrchestration {
     val channel = conn.createChannel
 
     // Declare exchange
-    val exchange = channel.exchangeDeclare(EXCHANGE_NAME_TODO, BuiltinExchangeType.FANOUT)
+    channel.exchangeDeclare(EXCHANGE_NAME_TODO, BuiltinExchangeType.FANOUT)
 
     // Declare queue
-    val queue = channel.queueDeclare(QUEUE_NAME, false, false, false, null)
+    channel.queueDeclare(QUEUE_NAME, false, false, false, null)
 
     // Queue Bind
     channel.queueBind(EXCHANGE_NAME_TODO, QUEUE_NAME, "")
@@ -65,48 +68,39 @@ object JobOrchestration {
           val payload = new String(body, "UTF-8")
           val spark_home = System.getenv("SPARK_HOME")
           val spark = sparkLauncher
-              .setSparkHome(spark_home)
+            .setSparkHome(spark_home)
             .addAppArgs(payload)
-            .setAppName("Job-"+payload)
+            .setAppName("Job-" + payload)
             .launch()
           spark.waitFor()
 
           // Post message do say that job is finish
           sendDoneJob(body)
 
-          /*val err = spark.getErrorStream
-          val fos = new FileOutputStream("/home/xnet/nath/err.txt")
-          println("Spark job fired up")
-          spark.waitFor()
-          var byte = new Array[Byte](1024)
-          var read = 0
-          while (read != -1) {
-            read = err.read(byte);
-            fos.write(byte,0,read)
-          }
-          println("status: "+ spark.exitValue())
-          println("Spark job finished")
         } catch {
           case e: Exception => println(e)
-        }*/
-
-        } catch { case  e: Exception => println(e) }
+        }
       }
+
     }
     channel.basicConsume(QUEUE_NAME, true, consumer)
   }
 
   def sendDoneJob(user: Array[Byte]) : Unit = {
-
-    // Create connection
-    val conn = cf.newConnection
-
-    // Get channel
-    val channel = conn.createChannel
-
-    // Declare exchange
-    val exchange = channel.exchangeDeclare(EXCHANGE_NAME_DONE, BuiltinExchangeType.FANOUT)
-
-    channel.basicPublish(EXCHANGE_NAME_DONE, "", null, user)
+    val loop = new Breaks
+    loop.breakable(
+      for(i <- 0 to 10)
+        try {
+          // Create connection
+          val conn = cf.newConnection
+          // Get channel
+          val channel = conn.createChannel
+          // Declare exchange
+          channel.exchangeDeclare(EXCHANGE_NAME_DONE, BuiltinExchangeType.FANOUT)
+          channel.basicPublish(EXCHANGE_NAME_DONE, "", null, user)
+          loop.break
+        } catch{
+          case e : Exception => println(e)
+        })
   }
 }
