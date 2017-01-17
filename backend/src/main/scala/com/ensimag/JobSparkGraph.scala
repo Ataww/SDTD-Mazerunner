f1fc31611df0de8b310b2e96aeefe304a417da97
RAKOTOARISOA Jérémy <jeremy.rakotoarisoa@gmail.com>
2017-01-17 15:03:53 +0100

    First iteration of Spark Job : Graph creation + Recommendations computing

--- backend/src/main/scala/com/ensimag/JobSparkGraph.scala
+++ backend/src/main/scala/com/ensimag/JobSparkGraph.scala
@@ -1,31 +1,37 @@
 package com.ensimag
 
 import org.apache.spark.graphx.Graph
+import org.apache.spark.util.IntParam
+// import classes required for using GraphX
+import org.apache.spark.graphx._
+import org.apache.spark.graphx.util.GraphGenerators
+import sys.process._
 
 /**
   * Created by julien on 06/12/16.
   */
 object JobSparkGraph {
 
-  def calculateNumberOfAirports(graph: Graph[String, Int]) {
-    // How many airports?
-    val numairports = graph.numVertices
-    println(numairports)
+  def calculateRecommendations(graph: Graph[String, String], username: String) {
+    /////// Linking the music from other users to the actual user ///////
+
+    // Récupération de la configuration pour spark
+    val sc = SparkInit.getSparkContext()
+
+    // Generate random seed
+    val r = scala.util.Random
+    val seed = r.nextLong()
+
+    // We simply choose random music liked by other users
+    val music_results = graph.collectNeighbors(EdgeDirection.In).takeSample(false, 30, seed)
+
+    // Add to id_titre the character t and map the result into an RDD
+    val lines = sc.parallelize(music_results.map {case (id, users) => "t" + id.toString})
+
+    // Saving result to HDFS into one file (called part-00000)
+    //TODO : HDFS Host has to be a variable
+    lines.repartition(1).saveAsTextFile("hdfs:////jobs_done/" + username)
+
   }
-
-  def calculateNumberOfRoutes(graph: Graph[String, Int]) {
-    // How many routes?
-    val numroutes = graph.numEdges
-    println(numroutes)
-  }
-
-  // Other possibilitie of JOB :
-
-  //graph.vertices.collect.foreach(println)
-  //graph.edges.collect.foreach(println)
-  //graph.edges.filter { case Edge(src, dst, prop) => prop > 1000 }.collect.foreach(println)
-  //graph.triplets.take(3).foreach(println)
-  //graph.triplets.sortBy(_.attr, ascending=false).map(triplet =>
-  //"Distance " + triplet.attr.toString + " from " + triplet.srcAttr + " to " + triplet.dstAttr + ".").collect.foreach(println)
 
 }
