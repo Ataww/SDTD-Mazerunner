55859f716d9f5a1b2692e7ca34fba6a0c5300437
Vincent Chenal <vince.chenal@gmail.com>
2017-01-18 15:19:59 +0100

    Update on Mazerunner API : almost finished ! (missing rabbitmq exceptions treatment + neo4j loadbalancer ?)

--- mazerunner/mazerunner_api.py
+++ mazerunner/mazerunner_api.py
@@ -2,20 +2,34 @@
 # coding: utf8
 
 import pika
-import json
 import logging
+import socket
 from pywebhdfs.webhdfs import PyWebHdfsClient
-from pywebhdfs.errors import PyWebHdfsException
-from requests.exceptions import ConnectionError, ActiveHostNotFound
+from pywebhdfs.errors import PyWebHdfsException, ActiveHostNotFound
+from requests.exceptions import ConnectionError
 from neo4j.v1 import GraphDatabase, basic_auth
 from flask import Flask, jsonify
-app = Flask(__name__)
 
+app = Flask(__name__)
+logger = logging.getLogger(__name__)
+
+'''
+CONFIGURATIONS VARIABLES
+'''
+
+# HDFS configuration
 hdfs_namenodes = ["hdfs-1", "hdfs-2"]
 
-# RabbitMQ
+# RabbitMQ configuration
 rabbitmq_host = "rabbitmq-1"
 rabbitmq_port = 5672
+
+# Neo4j configuration
+neo4j_username = "neo4j"
+neo4j_password = "neo4j_pass"
+neo4j_node = "neo4j-1"
+neo4j_bolt_port = "7687"
+
 
 @app.route("/compute_recommendation", defaults={'username': None})
 @app.route("/compute_recommendation/<username>")
@@ -23,87 +37,157 @@ def compute_recommendation(username):
     if username is None:
         return jsonify("There was no user specified")
 
-    logging.info("Received a computation job for user : "+username)
+    logger.info("Received a computation job for user : "+username)
 
-    # Get required graph from neo4j
-    logging.info("Getting datas from Neo4j...")
+    # Get data from neo4j
+    logger.info("Getting datas from Neo4j...")
+    read, records = get_data_from_neo4j(username)
+    if read is not True:
+        logger.error("There was a problem while trying to connect to Neo4j. Error :"+str(records))
+        return jsonify(records)
 
-    # Configure neo4j connection
-    records = get_data_from_neo4j(username)
-
-    # Write it to HDFS
-    logging.info("Writing the datas onto HDFS at this location : /jobs_to_do/"+username+".txt")
+    # Write it to HDFS in a certain format (csv)
+    logger.info("Writing the datas onto HDFS at this location : /jobs_to_do/"+username+".txt ...")
     written, error = write_data_to_hdfs(username, records)
-
     if written is not True:
-        return error
+        logger.error("There was an error while trying to Write the datas onto HDFS. Error "+str(error))
+        return jsonify(error)
 
     # Notify Spark via RabbitMQ that a subgraph is available
-    logging.info("Notifying Spark via RabbitMQ...")
+    logger.info("Notifying Spark via RabbitMQ...")
     notify_spark_via_rabbitmq(username)
 
     # Wait until the computing finish
-    logging.info("Wait for the end of calculation...")
+    logger.info("Wait for the end of calculation...")
     rabbitMQ_consumer = RabbitMQConsumerClient(username)
     rabbitMQ_consumer.start_consuming()
 
-    # Inject the computed result into neo4j
-    logging.info("Reading computing result...")
-    result = read_result_from_hdfs(username)
+    # Read the job result from HDFS
+    logger.info("Reading job result from HDFS...")
+    read, result = read_result_from_hdfs(username)
+    if read is not True:
+        logger.error("There was a problem while reading result from HDFS")
+        return result
 
+    # Inject result into neo4j
+    logger.info("Injecting result back to neo4j...")
+    injected, result = inject_recommendations_into_neo4j(username, result)
+    if injected is not True:
+        logger.error("There was a problem while injecting result into neo4j")
+        return jsonify(result)
+
+    logger.info("Job computation for username "+username+" is over")
     return jsonify("Job computation for username "+username+" is over")
 
+
 def get_data_from_neo4j(username):
-    neo4j_client = GraphDatabase.driver("bolt://149.202.170.185:7687", auth=basic_auth("neo4j", "neo4j_pass")).session()
-    result = neo4j_client.run("MATCH (n:Utilisateur)-[r1:AIME]->(t:Titre)<-[r2:AIME]-(n2:Utilisateur)-[r3:AIME]->(t2:Titre) WHERE n.nomUtilisateur = \""+username+"\" AND n2.nomUtilisateur <> \""+username+"\" AND t <> t2 RETURN DISTINCT  n2, type(r3), t2")
-    records = "n2,type(r3),t2\n"
-    for record in result:
-        n2 = record["n2"]
-        type = record["type(r3)"]
-        t2 = record["t2"]
+    to_return = {}
+    socket.setdefaulttimeout(10)
+    try:
+        logger.debug("Trying to connect to bolt://"+neo4j_node+":"+neo4j_bolt_port+" ...")
+        neo4j_client = GraphDatabase.driver("bolt://"+neo4j_node+":"+neo4j_bolt_port, auth=basic_auth(neo4j_username, neo4j_password)).session()
+        result = neo4j_client.run("MATCH (n:Utilisateur)-[r1:AIME]->(t:Titre)<-[r2:AIME]-(n2:Utilisateur)-[r3:AIME]->(t2:Titre) WHERE n.nomUtilisateur = \""+username+"\" AND n2.nomUtilisateur <> \""+username+"\" AND t <> t2 RETURN DISTINCT  n2, type(r3), t2")
+        records = "n2,type(r3),t2\n"
+        for record in result:
+            n2 = record["n2"]
+            type = record["type(r3)"]
+            t2 = record["t2"]
 
-        n2_str = "{\"nomUtilisateur\":\""+n2.get("nomUtilisateur")+"\",\"idUtilisateur\":\""+n2.get("idUtilisateur")+"\"}"
-        t2_str = "{\"idTitre\":\""+t2.get("idTitre")+"\",\"artiste\":\""+t2.get("artiste")+"\",\"nomChanson\":\""+t2.get("nomChanson")+"\"}"
+            # Generate GraphX needed format
+            records += n2.get("idUtilisateur")+","+n2.get("nomUtilisateur")+","+type+","+t2.get("idTitre")
 
-        records += n2_str+","+type+","+t2_str+"\n"
+        neo4j_client.close()
 
-    return records
+        return True, records
+    except socket.timeout as toe:
+        to_return["error"] = "There was a problem while trying to connect to Neo4j"
+        to_return["details"] = str(toe)
+        logger.debug(str(to_return))
+        return False, to_return
+
+    return False, None
+
+
+def inject_recommendations_into_neo4j(username, records):
+    to_return = {}
+    nb_relationships_created = 0
+    socket.setdefaulttimeout(10)
+    try:
+        logger.debug("Trying to connect to bolt://" + neo4j_node + ":" + neo4j_bolt_port + " ...")
+        neo4j_client = GraphDatabase.driver("bolt://" + neo4j_node + ":" + neo4j_bolt_port, auth=basic_auth(neo4j_username, neo4j_password)).session()
+
+        for record in records.decode("utf-8").split("\n"):
+            values = record.split(",")
+            track_id = values[len(values)-1]
+            if track_id:
+                result = neo4j_client.run("MATCH (u:Utilisateur {nomUtilisateur:'"+ username +"'}), (t:Titre {idTitre:'"+track_id+"'}) CREATE (u)-[:RECO]->(t)")
+                summary = result.consume()
+                logger.debug(str(summary.counters.relationships_created)+" recommendation added ("+username+" --RECO--> "+track_id+")")
+                nb_relationships_created += summary.counters.relationships_created
+
+        neo4j_client.close()
+
+        logger.info(str(nb_relationships_created)+" RECO relationships have been created")
+        return True, to_return
+    except socket.timeout as toe:
+        to_return["error"] = "There was a problem while trying to connect to Neo4j"
+        to_return["details"] = str(toe)
+        logger.debug(str(to_return))
+        return False, to_return
+
+    return False, None
+
 
 def write_data_to_hdfs(username, records):
-    # Configure WebHDFS connection
     global hdfs_namenodes
+    to_return = {}
+    file_path = "/jobs_to_do/"+username+".txt"
+    logger.debug("Writing file " + file_path + " to HDFS")
     try:
-        to_return = {}
+        logger.debug("Trying to connect to "+hdfs_namenodes[0]+" namenode")
+        hdfs_client = PyWebHdfsClient(host=hdfs_namenodes[0], port='50070', user_name='xnet', timeout=100)
+        hdfs_client.delete_file_dir(file_path)
+        hdfs_client.create_file(file_path, records.encode("utf-8"))
+    except (ConnectionError, PyWebHdfsException) as ce:
+        to_return["details_1"] = str(ce)
+        logger.debug("Failed connecting to" + hdfs_namenodes[0] + " namenode")
         try:
-            logging.info("Trying to connect to "+hdfs_namenodes[0]+" namenode")
-            hdfs_client = PyWebHdfsClient(host=hdfs_namenodes[0], port='50070', user_name='xnet', timeout=100)
-            hdfs_client.create_file("/jobs_to_do/"+username+".txt", records.encode("utf-8"))
-        except ConnectionError as ce:
-            to_return["details_1"] = str(ce)
-            try:
-                logging.info("Trying to connect to " + hdfs_namenodes[1] + " namenode")
-                hdfs_client = PyWebHdfsClient(host=hdfs_namenodes[1], port='50070', user_name='xnet', timeout=100)
-                hdfs_client.create_file("/jobs_to_do/" + username + ".txt", records.encode("utf-8"))
-            except ConnectionError as ce:
-                to_return["error"] = "There was a problem while trying to connect to HDFS namenode."
-                to_return["details_2"] = str(ce)
-                logging.info(json.dumps(to_return))
-                return False, jsonify(to_return)
-
-    except PyWebHdfsException as e:
-        to_return = {}
-        to_return["error"] = "There was a problem while trying to write neo4j request data into HDFS."
-        to_return["details"] = json.loads(e.msg.decode("utf-8"))
-        logging.info(json.dumps(to_return))
-        return False ,jsonify(to_return)
+            logger.debug("Trying to connect to " + hdfs_namenodes[1] + " namenode")
+            hdfs_client = PyWebHdfsClient(host=hdfs_namenodes[1], port='50070', user_name='xnet', timeout=100)
+            hdfs_client.delete_file_dir(file_path)
+            hdfs_client.create_file(file_path, records.encode("utf-8"))
+        except (ConnectionError, PyWebHdfsException) as ce:
+            to_return["error"] = "There was a problem while trying to connect to HDFS namenode."
+            to_return["details_2"] = str(ce)
+            logger.debug(str(to_return))
+            return False, to_return
 
     return True, None
 
+
 def read_result_from_hdfs(username):
-    # Configure WebHDFS connection
-    hdfs_client = PyWebHdfsClient(host=hdfs_namenodes[0], port='50070', user_name='xnet', timeout=100)
-    result = hdfs_client.read_file("/jobs_to_do/" + username + ".txt")
-    return result
+    result = ""
+    to_return = {}
+    file_path = "/jobs_done/"+username+"/part-00000"
+    logger.debug("Reading file " + file_path + " from HDFS")
+    try:
+        logger.debug("Trying to connect to " + hdfs_namenodes[0] + " namenode")
+        hdfs_client = PyWebHdfsClient(host=hdfs_namenodes[0], port='50070', user_name='xnet', timeout=100)
+        result = hdfs_client.read_file(file_path)
+    except (ActiveHostNotFound,ConnectionError) as e:
+        to_return["details_1"] = str(e)
+        try:
+            logger.debug("Trying to connect to " + hdfs_namenodes[1] + " namenode")
+            hdfs_client = PyWebHdfsClient(host=hdfs_namenodes[1], port='50070', user_name='xnet', timeout=100)
+            result = hdfs_client.read_file(file_path)
+        except (ActiveHostNotFound, ConnectionError) as e2:
+            to_return["error"] = "There was a problem while trying to read result from HDFS."
+            to_return["details2"] = str(e2)
+            logger.debug(str(to_return))
+            return False, to_return
+
+    return True, result
+
 
 def notify_spark_via_rabbitmq(username):
     # Configure RabbitMQ connection
@@ -112,6 +196,7 @@ def notify_spark_via_rabbitmq(username):
     rabbitmq_sending_channel = sending_connection.channel()
     rabbitmq_sending_channel.exchange_declare(exchange='jobs_to_do', type='fanout')
     rabbitmq_sending_channel.basic_publish(exchange='jobs_to_do', routing_key='', body=username)
+
 
 class RabbitMQConsumerClient():
     def __init__(self, username):
@@ -133,14 +218,15 @@ class RabbitMQConsumerClient():
         self.receiving_channel.start_consuming()
 
     def on_new_job_done(self, ch, method, props, body):
-        logging.info(body.decode("utf-8")+" == "+self.username+" ? %r" % (body.decode("utf-8") == self.username))
+        logger.debug("Comparing done job and username : "+body.decode("utf-8")+" == "+self.username+" ? %r" % (body.decode("utf-8") == self.username))
         if(body.decode("utf-8") == self.username):
             self.receiving_channel.stop_consuming()
 
 
 if __name__ == "__main__":
     # Configure logging
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s :: %(levelname)s :: %(message)s")
+    logging.basicConfig(level=logging.ERROR, format="%(asctime)s :: %(levelname)s :: %(message)s")
+    logging.getLogger(__name__).setLevel(logging.DEBUG)
 
     # Run the service
     app.run(threaded=True)
