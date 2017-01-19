81c1d796a07df22f2b8c534c042311ba12d02790
Maxime Hagenbourger <maximehg@gmail.com>
2017-01-19 12:42:54 +0100

    lul start_web_site????

--- application/script/start_web_site.py
+++ application/script/start_web_site.py
@@ -8,7 +8,7 @@ def launch_web_site():
     print("#############################################################")
     print("#######      Launch the web-site on the machine      ########")
     print("#############################################################")
-    subprocess.run(['npm','start'],cwd='/home/xnet/SDTD-Mazerunner/application/',stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
+    subprocess.run(['npm','start'],cwd='/home/xnet/SDTD-Mazerunner/application',stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
     return
 
 if __name__ == '__main__':
