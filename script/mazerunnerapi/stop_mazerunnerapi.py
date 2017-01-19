aed7c43dd2dd28a14c2037a516308104331e293f
Julien Sergent <sergent.julien.1@gmail.com>
2017-01-19 16:12:31 +0100

    update deployment for mazerunner api

--- script/mazerunnerapi/stop_mazerunnerapi.py
+++ script/mazerunnerapi/stop_mazerunnerapi.py
@@ -3,15 +3,16 @@
 import logging
 from subprocess import run
 from logging import info
+import subprocess
 
 home = '/home/xnet'
 mazerunner_api_script = home + "/SDTD-Mazerunner/mazerunner/mazerunnerapi.py"
-mazerunner_api_dir = home + '/mazerunnerapi' # contains the api script
+mazerunner_api_dir = home + '/mazerunnerapi'  # contains the api script
 
 
 def stop():
     info('Stopping Mazerunner API')
-    run(['rm -f /home/xnet/.neo4j/known_hosts'])
+    run(['rm', '-f', '/home/xnet/.neo4j/known_hosts'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     run(['sudo', 'systemctl', 'stop', 'mazerunnerapi'], check=True)
 
 
